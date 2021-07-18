7f8e6c4c9db43ca064b1e12eb46428a262ea2041
koki226 <k.te0421.baseball@ezweb.ne.jp>
2021-07-18 09:38:09 +0000

    Add password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  host = '<hex string>.vfs.cloud9.us-east-2.amazonaws.com'     # クラウドIDE
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -59,12 +62,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  
-  # cloud9への接続を許可する
+
+  # Cloud9 への接続を許可する
   config.hosts.clear
-  
-  config.action_mailer.raise_delivery_errors = false
-  
-  host = '<hex string>.vfs.cloud9.us-east-2.amazonaws.com'     # クラウドIDE
-　config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 end
